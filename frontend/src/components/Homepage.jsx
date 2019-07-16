477bf879abef0e0c1f39590d5ecae86b63c9f9b7
Sabine Robart <sabine.robart@protonmail.com>
2019-07-16 22:25:05 +0200

    [Style] homepage general styling (add background, sizes & colors) + [Add] CTA button

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,23 +1,32 @@
 import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
 import "../style/Homepage.scss";
+import { Link } from "react-router-dom";
 
 class Homepage extends Component {
   render() {
     return (
       <Row className="homepage-container m-0">
-        <Col md="4" className="text-right">
+        <Col md="5" className="text-right">
           <h1>
             Wildest
             <br />
             Circus
           </h1>
         </Col>
-        <Col md="7" className="m-5 p-5">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-          aliquam aspernatur, ipsum, reprehenderit quidem, ab provident sit sint
-          tenetur id praesentium unde est quam dolor omnis reiciendis quasi!
-          Ipsum, pariatur?
+        <Col md="6" className="m-5 p-5">
+          <div>
+            <div className="separation" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+              aliquam aspernatur, ipsum, reprehenderit quidem, ab provident sit
+              sint tenetur id praesentium unde est quam dolor omnis reiciendis
+              quasi! Ipsum, pariatur?
+            </p>
+          </div>
+          <Link to="/prestations" className="CTA">
+            Go now !
+          </Link>
         </Col>
       </Row>
     );
