2cc9832896e898332f3d3a22f403a775e8213ee7
Sabine Robart <sabine.robart@protonmail.com>
2019-07-16 16:34:23 +0200

    [Add] frontend routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./App.scss";
+import { Route, Switch } from "react-router-dom";
+import CircusNav from "./components/CircusNav";
+import Homepage from "./components/Homepage";
+// import Activities from "./components/Activities";
+// import Favorites from "./components/Favorites";
+// import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div>
-      <header className="App-header">Toto</header>
-    </div>
+    <React.Fragment>
+      <header>
+        <CircusNav className="App-header" />
+      </header>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        {/* <Route path="/Prestations" component={Activities} />
+        <Route path="/Favoris" component={Favorites} />
+        <Route path="/Profil" component={Profile} /> */}
+      </Switch>
+    </React.Fragment>
   );
 }
 
