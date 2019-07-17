1003b94cfb25b8efe715ca1a3e454cb2cef9c374
Sabine Robart <sabine.robart@protonmail.com>
2019-07-17 12:01:02 +0200

    [Add] backend route for login feature

--- backend/server.js
+++ backend/server.js
@@ -100,6 +100,25 @@ app.get("/favs/:idAuthor", (req, res) => {
   );
 });
 
+// Login
+
+app.post("/login", (req, res) => {
+  const user = req.body.nickname;
+  db.query(
+    `SELECT id, nickname, is_admin FROM user WHERE nickname="${user}"`,
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send("error when getting user route");
+      }
+      if (!rows) {
+        return res.status(404).send("No user found");
+      }
+      res.status(200).send(rows[0]);
+    }
+  );
+});
+
 app.listen(portNumber, () => {
   console.log(`API root available at: http://localhost:${portNumber}/`);
 });
